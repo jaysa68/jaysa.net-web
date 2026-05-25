73edbc5b6125ab595c945b82b2544c10ce3a0061
jaysa68 <jaysagarcia@berkeley.edu>
2026-05-25 00:57:16 -0700

    star emoticons

--- jaysa.net/src/app/emoticons/page.js
+++ jaysa.net/src/app/emoticons/page.js
@@ -9,6 +9,10 @@ export default function Home() {
       <br /><br />
       ╭─༺♡༻─╮
       <br /><br />
+      ⋆｡°✩☾⋆｡°✩
+      <br /><br />
+      ☾⋆⁺₊✧
+      <br /><br />
       (っ◔◡◔)っ 🍔
       <br /><br />
       ( ͠° ͟ʖ ͡°)
